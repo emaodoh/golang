13ce589732a98c69cb0906bd9dee2bdde674ef59
emaodoh <bybitonly419@gmail.com>
2026-06-14 20:00:57 +0100

    hashcode

---
+++ elementry-programing-go/hashcode/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func HashCode(dec string) string {
+	word := ""
+	for _, char := range dec {
+		hash := (char + rune(len(dec))) % 127
+
+		if hash < 33 {
+			hash += 33
+		}
+		word += string(hash)
+	}
+	return word
+}
+
+func main() {
+	fmt.Println(HashCode("A"))
+	fmt.Println(HashCode("AB"))
+	fmt.Println(HashCode("BAC"))
+	fmt.Println(HashCode("Hello World"))
+}
